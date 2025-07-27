0d105bb1489da38f4dedece3f856fcca7d23eac5
BRAYKENZ <rheycray02@gmail.com>
2025-07-28 03:04:22 +0800

    Add files via upload

---
+++ products.ts
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    title: "Freedom of Life 369 (White)",
+    description: "Oversize streetwear T-Shirt with bold print",
+    price: "Rp 149.000",
+    image: "/images/produk-1.jpg",
+  },
+  {
+    title: "Worldvibes Logo Tee (Black)",
+    description: "Premium cotton shirt with Worldvibes branding",
+    price: "Rp 139.000",
+    image: "/images/produk-2.jpg",
+  },
+  {
+    title: "Anti Prampuanq Friendly (White)",
+    description: "Statement streetwear T-Shirt with bold back print",
+    price: "Rp 155.000",
+    image: "/images/produk-3.jpg",
+  },
+  {
+    title: "Freedom of Life 369 (Black)",
+    description: "Dark version of our classic oversized tee",
+    price: "Rp 149.000",
+    image: "/images/produk-4.jpg",
+  },
+  {
+    title: "Worldvibes Street Look",
+    description: "Basic urban outfit in all black edition",
+    price: "Rp 129.000",
+    image: "/images/produk-5.jpg",
+  },
+  {
+    title: "Worldvibes Unity Pack",
+    description: "Group edition shirt pack",
+    price: "Rp 399.000",
+    image: "/images/produk-6.jpg",
+  }
+];
